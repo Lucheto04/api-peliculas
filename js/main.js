59d4d3f49749b9ae629229d3e2cf3de0587a9967
lucho <lucho.lgrg04@gmail.com>
2023-05-21 18:54:11 -0500

    feat: :construction: Obtenemos la informacion de todos los resultados
    Obtenemos ya la informacion detallada (titulo, duracion, tipo, img) de todos los resultados que dejen nuestra busqueda

--- js/main.js
+++ js/main.js
@@ -4,23 +4,38 @@ document.querySelector(".buscar").addEventListener("click", (e) => {
     e.preventDefault();
     const buscar = document.getElementById('inputFormulario').value;
     const url2 = `https://watchmode.p.rapidapi.com/autocomplete-search/?search_value=${buscar}&search_type=1`;
-
-    console.log(buscar);
-    
     const fetchData = async () =>{
         try {
             const response = await fetch(url2, options);
             const data = await response.json();
-            console.log(data);
+            // console.log(data); // Solicitamos los datos de la API
+            const resultadosBusqueda = data.results
+            obtenerID(resultadosBusqueda)
         } catch (error) {
             console.error(error);
         }
     }
     fetchData()
-    
 });
+function obtenerID(resultadosBusqueda) {
+    for (let i = 0; i < resultadosBusqueda.length; i++) {
+        const ID = resultadosBusqueda[i].id;
+    const url_info_detallada = `https://watchmode.p.rapidapi.com/title/${ID}/details/?language=ES`;
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url_info_detallada, options)
+                const data = await response.json();
+                // console.log(data); // obtenemos la info detallada de todos los resultados
 
 
+
+            } catch (error) {            
+            }
+        };
+        fetchData();
+    } 
+}
 
 
 
